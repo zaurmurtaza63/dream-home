e33c4270d6072ee84f1cae0ea99a6f96690b04b8
Murtaza <zaurmurtaza63@gmail.com>
2025-10-21 14:32:54 -0700

    solve the syntax issues and modules issues

--- home-town/src/api/auth.js
+++ home-town/src/api/auth.js
@@ -15,3 +15,16 @@ export const forgotPassword = (data) => API.post("/forgot-password", data);
 
 // Reset Password API
 export const resetPassword = (data) => API.post("/reset-password", data);
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+export async function loginUser(email, password) {
+  const res = await fetch(`${API_URL}/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
+
+  return await res.json();
+}
+
--- homeTown-backend/src/db.js
+++ homeTown-backend/src/db.js
@@ -6,6 +6,6 @@ dotenv.config();
 export const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASSWORD || "janiiii1234",
   database: process.env.DB_NAME || "hometown_db",
 });
