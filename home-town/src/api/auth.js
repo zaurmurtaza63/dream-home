a0c835bd530e70dd35f7bd5235653b1f7d4e2650
Murtaza <zaurmurtaza63@gmail.com>
2025-10-21 15:32:43 -0700

    make connection between frontend backend live

--- home-town/src/api/auth.js
+++ home-town/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth", // Your backend route prefix
+  baseURL: `${process.env.REACT_APP_API_URL}/auth`,  // Your backend route prefix
 });
 
 // Signup API
